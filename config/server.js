0ee29475e25a3fbdf0132564bfd91197fccc4068
YilberG <yilber-99@hotmail.com>
2020-10-28 09:59:09 -0500

    api parcial - Yilber Guevara - Willian Leguizamon

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '759bb8bafab4cdd48407189333224e5a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '70efbd92-fc6b-4e08-aa97-46eecc1a5dd9'
+};
